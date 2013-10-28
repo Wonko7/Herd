79a1d536a70fcb9284768b0ae473c63eb6413f96
Wonko7 <Wonko7@gmail.com>
2013-10-28 21:59:39 +0100

    tcp server

--- src/aqua_node/core.cljs
+++ src/aqua_node/core.cljs
@@ -3,47 +3,18 @@
             [cljs.nodejs :as node]))
 
 (defn -main [& args]
-  (let [net       (node/require "net")
-        handler   (fn [data]
-                    (println "handler used!")
-                    (this-as self
-                             (let [socket (.connect net (-> self .-target .-port) (-> self .-target .-ip) #(this-as t (.write t data)))]
-                               (-> socket
-                                 (.setEncoding "utf8")
-                                 (.on "data" #(.write self %))))))
-        handler #(println %)
-        socks-srv ((node/require "sockserver") handler)
+  (let [net     (node/require "net")
+        recv    (fn [c data]
+                  (println (str "recv: " data))
+                  (.write c "lolz\n"))
+        srv     (.createServer net (fn [c] 
+                                     (println "conn start")
+                                     (-> c 
+                                     (.on "end" #(println "conn end")) 
+                                     (.on "error" #(println "conn error")) 
+                                     (.on "data" (partial recv c)) 
+                                     (.write "lol\n"))))
         ]
-    (.listen socks-srv 6666 #(println (str "new conn: " (-> socks-srv .address .-port))))
-    )
-  (doseq [i (range 10)]
-    (println (str "Hello Aqua " i))))
- 
+    (.listen srv 6666 #(println (str "listening on: " (-> srv .address .-port))))))
+
 (set! *main-cli-fn* -main)
-
-
-;; var socks = require('sockserver'),
-;;     net = require('net');
-;; 
-;; function handler(data) {
-;;   var self = this,
-;;       socket = net.connect(
-;;         this.target.port,
-;;         this.target.ip || this.target.domain,
-;;         function () {
-;;           this.write(data);
-;;         }
-;;       );
-;; 
-;;   socket.setEncoding('utf8');
-;;   socket.on('data', function (chunk) {
-;;     self.write(chunk);
-;;   });
-;; }
-;; 
-;; var server = socks(handler);
-;; 
-;; server.listen(8000, function () {
-;;   console.log('Listening on port %d', server.address().port);
-;; });
-;; 
