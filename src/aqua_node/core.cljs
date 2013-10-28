8ecd8652cbbfb3f1eafe0bbaeb72ca49cbe5fb11
Wonko7 <Wonko7@gmail.com>
2013-10-29 00:16:04 +0100

    handle request state (ipv4 only)

--- src/aqua_node/core.cljs
+++ src/aqua_node/core.cljs
@@ -12,73 +12,74 @@
   (swap! connections assoc-in [conn :state] s)
   conn)
 
+(defn set-conn-dest [conn dest]
+  (println (str dest))
+  (swap! connections assoc-in [conn :dest] dest)
+  conn)
+
 (defn rm-conn [conn]
   (swap! connections dissoc conn)
   conn)
 
-(defn kill-conn [conn]
-  (println "killing a conn")
+(defn kill-conn [conn & [err]]
+  (println (str "killing a conn: " (or err "unknow")))
   (-> conn rm-conn .destroy))
 
-(defn handshake [c data]
-  (println "entered hs")
-  (let [len   (.-length data)
-        b     #(.readUInt8 data %)
-        state (:state (@connections c))]
-    (if (> len 2)
-      (let [socks-vers (b 0)
-            nb-auth-methods (b 1)
-            no-auth? (some zero? (map b (range 2 (min len (+ 2 nb-auth-methods)))))]
-        (if no-auth? 
-          (do 
-            (println (str "no auth " nb-auth-methods " -" state "-"))
-            (-> c 
-              (set-conn-state :request)
-              (.write (js/Buffer. (cljs/clj->js [0x05, 0x00])))))
-          (kill-conn c)))
-      (kill-conn c))))
 
-(defn request [c data]
-  (let [len (.-length data)]
-    (println len)
-    (doseq [i (range len)]
-      (println (str "buf: " (.readUInt8 data i))))))
 
+;; FIXME: most kill-conns should be wait for more data.
 (defn socks-recv [c data]
-  (let [state (:state (@connections c))]
+  (let [len        (.-length data)
+        b          #(.readUInt8 data %)
+        b16        #(.readUInt16BE data %)
+        socks-vers (b 0)       
+        state      (:state (@connections c))
+        ;; handle socks states:
+        handshake  (fn [c data]
+                     (println "entered hs")
+                     (if (> len 2)
+                       (let [nb-auth-methods (b 1)
+                             no-auth? (some zero? (map b (range 2 (min len (+ 2 nb-auth-methods)))))]
+                         (if no-auth? 
+                           (-> c 
+                             (set-conn-state :request)
+                             (.write (js/Buffer. (cljs/clj->js [0x05, 0x00]))))
+                           (kill-conn c "bad auth method")))
+                       (kill-conn c "too small")))
+        request    (fn [c data]
+                     (println "entered rq")
+                     (if (> len 4)
+                       (let [cmd       (b 1)
+                             addr-type (b 3)]
+                         (doseq [i (range len)]
+                           (println (str "buf: " (.readUInt8 data i))))
+                         (if (not= cmd 1) 
+                           (kill-conn c "bad request command")
+                           (condp = addr-type
+                             1 (if (< len 10)
+                                 (kill-conn c "ip4 info too small")
+                                 (-> c
+                                   (set-conn-dest {:type :ip4 :addr (apply str (interpose "." (map b (range 4 8)))) :port (b16 8)})
+                                   (set-conn-state :relay))))))
+                       (kill-conn c "too small")))
+        ]
     (println (str "state: " state))
-    (condp = state
-      :handshake   (handshake c data)
-      :request     (request c data)
-      (kill-conn c))))
+    (if (not= socks-vers 5)
+      (kill-conn c "bad socks version")
+      (condp = state
+        :handshake   (handshake c data)
+        :request     (request c data)
+        (kill-conn c)))))
 
 (defn -main [& args]
   (let [net     (node/require "net")
-        recv    (fn [c data]
-                  (let [len (.-length data)
-                        b   #(.readUInt8 data %)
-                        state (:state (@connections c))]
-                    (when (> len 2)
-                      (let [socks-vers (b 0)
-                            nb-auth-methods (b 1)
-                            no-auth? (some zero? (map b (range 2 (+ 2 nb-auth-methods))))]
-                        (println @connections)
-                        (if no-auth? 
-                          (println (str "no auth " nb-auth-methods " -" state "-"))
-                          (kill-conn c)))))
-                  (println (str "recv: " data))
-                  (doseq [i (range (.-length data))]
-                    (println (str "buf: " (.readUInt8 data i))))
-                  (.write c "lolz\n"))
         srv     (.createServer net (fn [c] 
                                      (println "conn start")
                                      (-> c 
                                        (add-conn)
                                        (.on "end" #(println "conn end")) 
-                                       (.on "error" #(println "conn error")) 
-                                       (.on "data" #(socks-recv c %)) 
-                                       )))
-        ]
+                                       (.on "error" kill-conn) 
+                                       (.on "data" #(socks-recv c %)))))]
     (.listen srv 6666 #(println (str "listening on: " (-> srv .address .-port))))))
 
 (set! *main-cli-fn* -main)
