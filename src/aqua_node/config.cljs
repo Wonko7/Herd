a4fc0ca1379420860e66940adbc7f93954625a9b
Wonko7 <Wonko7@gmail.com>
2013-10-29 16:59:58 +0100

    add config system

---
+++ src/aqua_node/config.cljs
@@ -0,0 +1,11 @@
+(ns aqua-node.config
+  (:require [cljs.core :as cljs]
+            [cljs.nodejs :as node]
+            [cljs.reader :as reader]))
+
+(def config (atom {}))
+
+(defn read-config []
+  (let [fs   (node/require "fs")
+        read #(swap! config merge (reader/read-string %))]
+    (read (.readFileSync fs "aquarc" "utf8"))))
--- src/aqua_node/core.cljs
+++ src/aqua_node/core.cljs
@@ -1,9 +1,11 @@
 (ns aqua-node.core
   (:require [cljs.core :as cljs]
             [cljs.nodejs :as node]
-            [aqua-node.socks :as socks]))
+            [aqua-node.socks :as socks]
+            [aqua-node.config :as config]))
 
 (defn -main [& args]
-  (socks/create-socks-server {:port 6666}))
+  (let [config (config/read-config)]
+    (socks/create-socks-server (:server config))))
 
 (set! *main-cli-fn* -main)
