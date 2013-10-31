778653008d43bc2302337ae3fab489749c3234cb
Wonko7 <Wonko7@gmail.com>
2013-10-31 20:19:06 +0100

    Make first connection dtls work

--- src/aqua_node/core.cljs
+++ src/aqua_node/core.cljs
@@ -7,9 +7,14 @@
 
 (defn -main [& args]
   (let [config (config/read-config)]
-    (dtls/create-aqua-listening-socket (:server config) #(println "new dtls conn on: " (.remoteAddress %) ":" (.remotePort %)))
-    (socks/create-socks-server (:server config))))
+    (if (some #(not= % :app-proxy) (-> config :server :type))
+      (dtls/create-aqua-listening-socket (:server config) #(println "new dtls conn on: " (.-remoteAddress %) ":" (.-remotePort %)))
+      (do
+        (println "app-proxy only")
+        (socks/create-socks-server (:server config))
+        (dtls/connect-to-aqua-node (:dir-server config) (:server config) #(println "conn?"))))))
 
+;(set! *main-cli-fn* -main)
 (set! *main-cli-fn* #(try
                        (apply -main %&)
                        (catch js/Object e (println "/!\\  I don't know what I excepted: " e))))
--- src/aqua_node/dtls.cljs
+++ src/aqua_node/dtls.cljs
@@ -2,13 +2,17 @@
   (:require [cljs.core :as cljs]
             [cljs.nodejs :as node]))
 
-(defn create-aqua-listening-socket [{auth :auth
-                                     {addr :addr
-                                      port :port} :listen}
+(defn mk-dtls [auth addr]
+  (let [fs          (node/require "fs")
+        cat         (fn [k] (try {k (.readFileSync fs (auth k) "utf8")}
+                              (catch js/Object e (println "/!\\  could not load auth info: " e))))]
+    [(node/require "nodedtls") (cljs/clj->js (merge auth {:host addr} (cat :key) (cat :cert)))]))
+
+(defn create-aqua-listening-socket [{auth :auth addr :addr port :port}
                                     new-conn-handler]
-  (let [dtls       (node/require "nodedtls")
-        fs         (node/require "fs")
-        cat        (fn [k] (try {k (.readFileSync fs (auth k) "utf8")}
-                             (catch js/Object e (println "/!\\  could not load auth info: " e)))) 
-        auth       (merge auth (cat :key) (cat :cert))]
-    (.createServer dtls (cljs/clj->js auth) new-conn-handler)))
+  (let [[dtls auth] (mk-dtls auth addr)]
+    (.createServer dtls port auth new-conn-handler))) ;; addr goes in options?
+
+(defn connect-to-aqua-node [{addr :addr port :port} {auth :auth} conn-handler]
+  (let [[dtls auth] (mk-dtls auth addr)]
+    (.connect dtls port 0 auth conn-handler)))
