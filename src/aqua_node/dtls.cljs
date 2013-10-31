ab20859023c3dc8f5f0764940de76ac99a43da95
Wonko7 <Wonko7@gmail.com>
2013-10-31 22:00:03 +0100

    use something closer to tls api with dtls

--- src/aqua_node/dtls.cljs
+++ src/aqua_node/dtls.cljs
@@ -2,17 +2,17 @@
   (:require [cljs.core :as cljs]
             [cljs.nodejs :as node]))
 
-(defn mk-dtls [auth addr]
+(defn mk-dtls [auth addr port]
   (let [fs          (node/require "fs")
         cat         (fn [k] (try {k (.readFileSync fs (auth k) "utf8")}
                               (catch js/Object e (println "/!\\  could not load auth info: " e))))]
-    [(node/require "nodedtls") (cljs/clj->js (merge auth {:host addr} (cat :key) (cat :cert)))]))
+    [(node/require "nodedtls") (cljs/clj->js (merge auth {:host addr} {:port port} (cat :key) (cat :cert)))]))
 
 (defn create-aqua-listening-socket [{auth :auth addr :addr port :port}
                                     new-conn-handler]
-  (let [[dtls auth] (mk-dtls auth addr)]
-    (.createServer dtls port auth new-conn-handler))) ;; addr goes in options?
+  (let [[dtls opts] (mk-dtls auth addr port)]
+    (.createServer dtls port opts new-conn-handler))) ;; FIXME: based on tls api, this is not what a nice dtls api should look like.
 
 (defn connect-to-aqua-node [{addr :addr port :port} {auth :auth} conn-handler]
-  (let [[dtls auth] (mk-dtls auth addr)]
-    (.connect dtls port 0 auth conn-handler)))
+  (let [[dtls opts] (mk-dtls auth addr port)]
+    (.connect dtls opts conn-handler)))
