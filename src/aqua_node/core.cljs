5f18d13444e245e42cf97c4841f7ace5459ac540
William <William@undefined.re>
2013-11-01 14:12:40 +0100

    hardcoded dtls srv & client test

--- src/aqua_node/core.cljs
+++ src/aqua_node/core.cljs
@@ -5,14 +5,25 @@
             [aqua-node.socks :as socks]
             [aqua-node.config :as config]))
 
+(defn new-dtls-conn [s]
+  (println "new dtls conn on: " (.-remoteAddress s) ":" (.-remotePort s))
+  (.on s "data" (fn [b]
+                  (println "recv: " (.toString b))
+                  (.write s (str "polly wants a cracker! " (.toString b))))))
+
+(def i (atom 0))
+(defn write [s]
+  (js/setInterval #(.write s (str "hello" (swap! i inc))) 1000))
+
 (defn -main [& args]
   (let [config (config/read-config)]
     (if (some #(not= % :app-proxy) (-> config :server :type))
-      (dtls/create-aqua-listening-socket (:server config) #(println "new dtls conn on: " (.-remoteAddress %) ":" (.-remotePort %)))
+      (dtls/create-aqua-listening-socket (:server config) new-dtls-conn)
       (do
         (println "app-proxy only")
         (socks/create-socks-server (:server config))
-        (dtls/connect-to-aqua-node (:dir-server config) (:server config) #(println "conn?"))))))
+        (let [s (dtls/connect-to-aqua-node (:dir-server config) (:server config) identity)]
+          (write s))))))
 
 ;(set! *main-cli-fn* -main)
 (set! *main-cli-fn* #(try
