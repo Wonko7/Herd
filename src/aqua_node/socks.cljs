de9759ae32a4ab87cbb3e28a026750fd5b6e4d04
William <William@undefined.re>
2013-11-03 17:21:21 +0100

    indent style

--- src/aqua_node/socks.cljs
+++ src/aqua_node/socks.cljs
@@ -22,8 +22,8 @@
                              no-auth? (some zero? (map b (range 2 (min len (+ 2 nb-auth-methods)))))]
                          (if no-auth?
                            (-> c
-                             (c/update-data [:socks :state] :request)
-                             (.write (js/Buffer. (cljs/clj->js [0x05, 0x00]))))
+                               (c/update-data [:socks :state] :request)
+                               (.write (js/Buffer. (cljs/clj->js [0x05, 0x00]))))
                            (kill-conn c "bad auth method")))
                        (kill-conn c "too small")))
         request    (fn [c data]
@@ -43,8 +43,8 @@
                            (if too-short? ;; to-[ip/port] are functions to avoid executing the code if not enough data
                              (kill-conn c (str "not enough data. conn type: " type))
                              (-> c
-                               (c/update-data [:socks :dest] {:type type :addr (to-ip) :port (to-port)})
-                               (c/update-data [:socks :state] :relay)))))))]
+                                 (c/update-data [:socks :dest] {:type type :addr (to-ip) :port (to-port)})
+                                 (c/update-data [:socks :state] :relay) ))))))]
     (if (not= socks-vers 5)
       (kill-conn c "bad socks version")
       (condp = state
@@ -57,10 +57,10 @@
         srv     (.createServer net (fn [c]
                                      (println (str "###  App-Proxy: new connection on: " (-> c .address .-ip) ":" (-> c .address .-port)))
                                      (-> c
-                                       (c/add {:socks {:state :handshake}})
-                                       (c/add-listeners {:end   #(println "###  App-Proxy: connection end: " (-> c .address .-ip) ":" (-> c .address .-port))
-                                                         :error kill-conn
-                                                         :data  #(socks-recv c new-conn-handler %)}))))
+                                         (c/add {:socks {:state :handshake}})
+                                         (c/add-listeners {:end   #(println "###  App-Proxy: connection end: " (-> c .address .-ip) ":" (-> c .address .-port))
+                                                           :error kill-conn
+                                                           :data  #(socks-recv c new-conn-handler %)}))))
         new-srv #(println "###  App-Proxy listening on:" (-> srv .address .-ip) ":" (-> srv .address .-port))]
     (if addr
       (.listen srv port addr new-srv)
