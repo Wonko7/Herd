3498177418e42eff50fb489f3ff8e56d788a809c
William <William@undefined.re>
2013-11-06 13:30:17 +0100

    add ntor file, references in comments

---
+++ src/aqua_node/ntor.cljs
@@ -0,0 +1,9 @@
+(ns aqua-node.ntor
+  (:require [cljs.core :as cljs]
+            [cljs.nodejs :as node]))
+
+
+;; see: torspec/proposals/216-ntor-handshake.txt
+;;      torspec/tor-spec.txt 5.1.4
+
+;; (defn )
