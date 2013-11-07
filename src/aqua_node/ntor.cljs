af0e8b7034633cd127ac303a55d2b0438c4dd59a
William <William@undefined.re>
2013-11-07 15:07:33 +0100

    buffer manipulation helpers, ntor conf

--- src/aqua_node/ntor.cljs
+++ src/aqua_node/ntor.cljs
@@ -9,18 +9,26 @@
 ;; FIXME: buff helpers: this will be exported.
 (defn cct [& bs]
   (js/Buffer.concat (cljs/clj->js bs)))
+
+(defn b= [a b]
+  (= (.toString a "ascii") (.toString b "ascii")))
+
+(defn b-cut [b & xs]
+  (doall (map #(.slice b %1 %2) (cons 0 xs) (concat xs [(.-length b)]))))
 ;; end buff helpers
 
 ;; FIXME: part or all of this static (non user) conf will get exported as pieces of it are needed by other modules.
 (def conf
   (let [protoid "ntor-curve25519-sha256-1"]
-    {:m-expand (str protoid ":key_expand")
-     :t-key    (str protoid ":key_extract")
-     :mac      (str protoid ":mac")
-     :verify   (str protoid ":verify")
-     :protoid  protoid
+    {:m-expand    (str protoid ":key_expand")
+     :t-key       (str protoid ":key_extract")
+     :mac         (str protoid ":mac")
+     :verify      (str protoid ":verify")
+     :protoid     protoid
      :node-id-len 20
-     }))
+     :key-id-len  32
+     :g-len       32
+     :h-len       32}))
 
 (defn hmac [key message]
   (let [crypto (node/require "crypto")]
