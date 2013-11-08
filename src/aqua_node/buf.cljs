b3beee87d5a84f854ebf6aa9b6010f917fbf0195
William <William@undefined.re>
2013-11-08 17:16:06 +0100

    add debug & structuring to buf helpers

--- src/aqua_node/buf.cljs
+++ src/aqua_node/buf.cljs
@@ -3,7 +3,10 @@
             [cljs.nodejs :as node]))
 
 
-;; node js/buffer helpers
+;; node js/buffer helpers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn new [data]
+  (js/Buffer. data))
 
 (defn cat [& bs]
   "concatenate buffers"
@@ -17,9 +20,8 @@
   "divide the buffer: (cut b 55 88 99) will return a seq of slices from 0 to 55, 55 to 88, 88 to end of buf"
   (map #(.slice b %1 %2) (cons 0 xs) (concat xs [(.-length b)])))
 
-(defn hx [b]
-  "debug helper"
-  (.toString b "hex"))
+
+;; low level io ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn mk-readers [b]
   "make big endian readers"
@@ -29,5 +31,12 @@
   "make big endian writers"
   [#(.writeUInt8 b %1 %2) #(.writeUInt16BE b %1 %2) #(.writeUInt32BE b %1 %2)])
 
-(defn new [data]
-  (js/Buffer. data))
+;; debug ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn hx [b]
+  "debug helper"
+  (.toString b "hex"))
+
+(defn print-x [b & [s]]
+  (println "---  " s (hx b)))
+
--- src/aqua_node/roles.cljs
+++ src/aqua_node/roles.cljs
@@ -1,18 +1,16 @@
 (ns aqua-node.roles
   (:require [cljs.core :as cljs]
             [cljs.nodejs :as node]
+            [aqua-node.buf :as b]
             [aqua-node.circ :as circ]
             [aqua-node.conns :as c]
             [aqua-node.conn-mgr :as conn]))
 
 
 ;; FIXME: the following are placeholders.
-(defn print-buf [b]
-  (println "---  recv:" (.toString b)))
-
 (defn forward [b]
   (let [[c cdata] (first (filter (fn [[c data]] (and (= (:type data) :aqua) (= (:cs data) :client))) (c/get-all)))]
-    (print-buf b)
+    (b/print b "recv:")
     (.write c b)))
 
 (defn new-dtls-conn [s]
@@ -24,18 +22,22 @@
 (def i (atom 0))
 
 (defn conn-to-dtls [s]
-  (c/add-listeners s {:data print-buf})
+  (c/add-listeners s {:data #(b/print % "recv:")})
   ;(js/setInterval #(.write s (str "hello" (swap! i inc))) 1000)
   )
 ;; FIXME: end placeholders.
 
-(defn is? [role roles]
+(defn is? [role roles] ;; FIXME -> when needed elsewhere move to roles
   (some #(= role %) roles))
 
 (defn bootstrap [{roles :roles ap :app-proxy-conn aq :aqua-conn ds :dir-server :as config}]
   (let [is?   #(is? % roles)]
     (println "###  Bootstrapping as" roles)
-    (conn/new :aqua :server aq config new-dtls-conn) ;; in the case of entry only, still needed for NW consensus polling.
+    (when
+      (conn/new :aqua :server aq config new-dtls-conn))
     (when (is? :app-proxy)
       (conn/new :socks :server ap config forward)
-      (conn/new :aqua  :client ds config conn-to-dtls))))
+      ;; the following will be covered by conn-to all known nodes --> sooooon
+      (conn/new :aqua  :client ds config conn-to-dtls))
+    (when (and false (not (is? :dir-server))) ;; dir-servs also need to connect to other dir-sers, we'll see about that when we get there.
+      (conn/new :aqua  :client ds config get-dir-info))))
