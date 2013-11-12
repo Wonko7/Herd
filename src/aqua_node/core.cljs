bb4be48e3d58e96441417fe6db9cb1f8a523960c
William <William@undefined.re>
2013-11-12 11:49:24 +0100

    remove handshake self test

--- src/aqua_node/core.cljs
+++ src/aqua_node/core.cljs
@@ -8,14 +8,6 @@
 
 (defn -main [& args]
   (let [config (config/read-config)]
-    ;; This has nothing to do here. But I like seeing that it works...
-    (let [srv-id                   (js/Buffer. "waaaaaaaaaaoooooooow" "ascii")
-          [srv-sec-b srv-pub-B]    (hs/gen-keys)
-          [auth-info create]       (hs/client-init  {:srv-id srv-id :pub-B srv-pub-B})
-          [srv-shared-sec created] (hs/server-reply {:node-id srv-id :pub-B srv-pub-B :sec-b srv-sec-b} create 72)
-          cli-shared-sec           (hs/client-finalise auth-info created 72)]
-      (println :cli (.toString cli-shared-sec "hex"))
-      (println :srv (.toString srv-shared-sec "hex")))
     (roles/bootstrap config)))
 
 ;(set! *main-cli-fn* -main)
