090b768b070fb5e6b667825ed09c6c7532ed2d72
William <William@undefined.re>
2013-12-21 14:42:09 +0100

    also cut packets coming from destination

--- src/aqua_node/circ.cljs
+++ src/aqua_node/circ.cljs
@@ -352,8 +352,9 @@
                      (update-data circ-id [:roles] (cons :exit (:roles circ)))
                      (let [dest (first (conv/parse-addr r-payload))
                            sock (if (= :tcp (:proto dest))
-                                  (conn/new :tcp :client dest config {:data  (fn [config soc buf]
-                                                                               (relay config socket circ-id :data :b-enc buf))
+                                  (conn/new :tcp :client dest config {:data  (fn [config soc b]
+                                                                               (doall (map (fn [b] (.nextTick js/process #(relay config socket circ-id :data :b-enc b)))
+                                                                                           (apply (partial b/cut b) (range 1350 (.-length b) 1350)))))
                                                                       :error #(do (log/error "closed:" dest) (destroy config circ-id))})
                                   (conn/new :udp :client nil config {:data  (fn [config soc msg rinfo]
                                                                               (let [data       (b/new (+ 10 (.-length msg)))
