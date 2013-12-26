8deb39af456a9ca983988e999caae483babc1513
William <William@undefined.re>
2013-12-26 18:08:35 +0100

    successfully receiving sdp offer from kamailio

--- src/aqua_node/rtpp.cljs
+++ src/aqua_node/rtpp.cljs
@@ -2,6 +2,7 @@
   (:require [cljs.core :as cljs]
             [cljs.nodejs :as node]
             [cljs.core.async :refer [chan <! >!]]
+            [clojure.string :as str]
             [aqua-node.log :as log]
             [aqua-node.buf :as b]
             [aqua-node.conns :as c])
@@ -11,12 +12,18 @@
   (println "error on rtpp server"))
 
 (defn process [config socket message rinfo]
-  (log/debug "RTP-Proxy: from:" (.-address rinfo) (.-port rinfo))
+  (log/debug "RTP-Proxy: from:" (.-address rinfo) (.-port rinfo) :msg (.toString message "ascii"))
   (let [bcode        (node/require "node-bencode")
-        [cookie cmd] (-> message (.toString "ascii") (.split " "))
-        cmd          (cljs/js->clj (.decode bcode cmd))]
+        [cookie cmd] (-> message (.toString "ascii") (str/split #" " 2))
+        cmd          (cljs/js->clj (.decode bcode cmd "ascii"))]
     (log/debug :cookie cookie :cmd (for [k (keys cmd)]
-                                     (list k (.toString (cmd k)))))))
+                                     (list k (.toString (cmd k)))))
+    (when (= (.toString (cmd "command")) "ping")
+      (let [ra    (.-address rinfo)
+            rp    (.-port rinfo)
+            reply (b/new (str cookie " " (.encode bcode (cljs/clj->js {:result "pong"}))))]
+        (println "replying to ping:" (.toString reply))
+        (.send socket reply 0 (.-length reply) rp ra)))))
 
 (defn create-server [{host :host port :port} config]
   (let [socket (.createSocket (node/require "dgram") "udp4")] ;; FIXME hardcoded to ip4 for now.
