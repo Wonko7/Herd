1c70db553dd602a2a234c86603371c520100dfd8
William <William@undefined.re>
2014-01-05 18:38:47 +0100

    add sdp answer processing. looks ok, but it's a draft

--- src/aqua_node/rtpp.cljs
+++ src/aqua_node/rtpp.cljs
@@ -21,35 +21,50 @@
         [cookie msg] (-> message (.toString "ascii") (str/split #" " 2))
         msg          (-> (.decode bcode msg "ascii") cljs/js->clj)
         cmd          (.toString (msg "command"))
-        mk-reply     #(do (log/debug "RTP-Proxy reply to" cookie ":" cmd "<" %)
+        mk-reply     #(do (log/error "RTP-Proxy reply to" cookie ":" cmd "<" %)
                           (b/new (str cookie " " (.encode bcode (cljs/clj->js %)))))
         send         #(.send socket % 0 (.-length %) (.-port rinfo) (.-address rinfo))
-        replace-sdp  (fn []
-                       (go (let [sdp          (.toString (msg "sdp"))
-                                 sdp-ch       (chan)
-                                 ip           (second (re-find #"(?m)c\=IN IP4 ((\d+\.){3}\d+)" sdp))
-                                 ports        (map next (re-seq #"(?m)m\=(\w+)\s+(\d+)" sdp))
-                                 circs        (repeatedly path/get-path)
-                                 change-port  (fn [sdp old new]
-                                                (str/replace sdp (re-pattern (str "(?m)(m\\=\\w+\\s+)" old)) #(str %2 new)))
-                                 assoc-circ   (fn [cid [media old]]
-                                                (go (let [[_ local-port] (<! (path/attach-local-udp4 config cid {:type :ip4 :proto :udp :host ip :port old}))
-                                                          dist-port      (-> cid circ/get-data :path-dest :port)]
-                                                      (swap! calls assoc-in [(msg "call-id") media] {:local-circ-port local-port
-                                                                                                     :distant-circ-port dist-port
-                                                                                                     :local-port old})
-                                                      [old dist-port])))
-                                 nsdp         (reduce #(go (let [sdp       (<! %1) 
-                                                                 [old new] (<! %2)]
-                                                             (change-port sdp old new)))
-                                                      sdp-ch (map assoc-circ circs ports))]
-                             (go (>! sdp-ch (str/replace sdp ip (-> (first circs) circ/get-data :path-dest :host))))
-                             (go (-> {:result "ok" :sdp (<! nsdp)} mk-reply send)))))]
+        ;; SDP & circ glue:
+        parse-sdp    #(let [sdp (.toString (msg "sdp"))]
+                        [sdp
+                         (second (re-find #"(?m)c\=IN IP4 ((\d+\.){3}\d+)" sdp))
+                         (map next (re-seq #"(?m)m\=(\w+)\s+(\d+)" sdp))])
+        change-port  (fn [sdp old new]
+                       (str/replace sdp (re-pattern (str "(?m)(m\\=\\w+\\s+)" old)) #(str %2 new)))
+        offer-sdp    (fn []
+                       (let [[sdp ip ports] (parse-sdp)        
+                             sdp-ch         (chan)
+                             circs          (repeatedly path/get-path)
+                             assoc-circ     (fn [cid [media old]]
+                                              (go (let [[_ local-port] (<! (path/attach-local-udp4 config cid {:type :ip4 :proto :udp :host ip :port old}))
+                                                        dist-port      (-> cid circ/get-data :path-dest :port)]
+                                                    (swap! calls assoc-in [(msg "call-id") media] {:local-circ-port local-port
+                                                                                                   :distant-circ-port dist-port
+                                                                                                   :local-port old
+                                                                                                   :circuit cid})
+                                                    [old dist-port])))
+                             replace-sdp    #(go (let [sdp       (<! %1)
+                                                       [old new] (<! %2)]
+                                                   (change-port sdp old new)))
+                             nsdp           (reduce replace-sdp sdp-ch (map assoc-circ circs ports))]
+                         (go (>! sdp-ch (str/replace sdp ip (-> (first circs) circ/get-data :path-dest :host))))
+                         (go (-> {:result "ok" :sdp (<! nsdp)} mk-reply send))))
+        answer-sdp   (fn []
+                       (let [[sdp ip ports] (parse-sdp)
+                             call-id        (msg "call-id")
+                             call-data      (@calls (msg "call-id"))
+                             circs          (for [[media port] ports
+                                                  :let [med (call-data media)]]
+                                              (do (swap! calls assoc-in [(msg "call-id") media :distant-port] port)
+                                                  [port (:local-circ-port med)]))
+                             sdp            (str/replace sdp ip (-> config :aqua :host))
+                             sdp            (reduce #(change-port %1 (first %2) (second %2)) sdp circs)]
+                         (-> {:result "ok" :sdp sdp} mk-reply send)))]
     (condp = cmd
       "ping"   (-> {:result "pong"} mk-reply send)
-      "offer"  (replace-sdp)
+      "offer"  (offer-sdp)
       "delete" (-> {:result "ok"} mk-reply send) ;; FIXME -> keep track of call ids so we can kill circs on delete.
-      "answer" (-> {:result "ok" :sdp (.toString (msg "sdp"))} mk-reply send)
+      "answer" (answer-sdp)
       (log/error "RTP-Proxy: unsupported command" cmd))))
 
 (defn create-server [{host :host port :port} config]
